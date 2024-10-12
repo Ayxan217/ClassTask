4afc86e15fe2d8add24c3a4bd6f76e1448ee9960
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-12 16:36:42 +0400

    task added

---
+++ Class_Task/Class_Task/Models/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Task.Models
+{
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool İsGraduated;
+       
+
+        public  void Getİnfo()
+        { 
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Group: {Group}, Point: {Point}");
+        }
+        public void CheckGradiation()
+        {
+            if(İsGraduated == false)
+            {
+                Console.WriteLine("mezun olmayıb");
+
+            }
+            else
+            {
+                Console.WriteLine("mezundur");
+            }
+        }
+        public  void GetDiplomaDegree()
+        {
+          while(İsGraduated)
+            {
+                if (Point < 65)
+                {
+                    Console.WriteLine("diplom yoxdur");
+                    break;
+                }
+                else if(Point>=65 && Point < 80)
+                {
+                    Console.WriteLine("adi diplom");
+                    break;
+                }
+                else if(Point>80 && Point <= 90)
+                {
+                    Console.WriteLine("şeref diplomu");
+                }
+                else if (Point >90)
+                {
+                    Console.WriteLine(" yüksek şeref diplomu");
+                }
+            }
+        }
+    }
+}
---
+++ Class_Task/Class_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using Class_Task.Models;
+
+namespace Class_Task
+{
+    public class Program:Student
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+                Name = "Ayxan",
+                Surname = "Mammadli",
+                Group = "TK-96",
+                Point = 78,
+                İsGraduated =false
+            };
+
+            student.Getİnfo();
+            student.CheckGradiation();
+            student.GetDiplomaDegree();
+
+
+        }
+    }
+}
